108040510180e500c7137d9cfc1559e504ccd2ce
dJonaitis <55477640+dJonaitis@users.noreply.github.com>
2025-02-13 20:37:24 +0100

    further work and MVP

---
+++ README.md
@@ -0,0 +1,6 @@
+# Lithuanian Parliament law analyser
+This app aims to be a way to increase transparency in the operations of parliament. It scrapes the laws being discussed from the website, and utilises [EuroLLM](https://arxiv.org/pdf/2409.16235v1).
+
+
+The basic flowchart of the app can be found below:
+![flowchart](flowchart.png).
--- web-app/app.py
+++ web-app/app.py
@@ -1,22 +1,30 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, session
+
 
 import pandas as pd
+import os
 
+# FLASK INITIALISATION
+PROJECT_ROOT = os.path.dirname(os.path.realpath(__file__))
+filename = pd.Timestamp.today().strftime('%Y-%m-%d') + '_laws.csv'
+DATABASE = os.path.join(PROJECT_ROOT, 'data', filename)
 
-#FLASK APP
+df = pd.read_csv(DATABASE)
+
 app = Flask(__name__)
 
-today = pd.Timestamp.today().strftime('%Y-%m-%d')
-today_laws = pd.read_csv(f'{today}_laws.csv')
+
+
 
 # ROUTING
 @app.route("/")
 def index():
-    return render_template("landing.html")
-
-
-
-
+    return render_template("index.html", 
+                           summary=df['summary'].tolist(), 
+                           title=df['Pavadinimas'].tolist(),
+                           author=df['Pateikė'].tolist(),
+                           date=df['Data'].tolist(),
+                           number=df['Numeris'].tolist())
 
 if __name__ == "__main__":
     app.run(port=3000, debug=True)
